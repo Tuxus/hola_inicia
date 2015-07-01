555cda03a27ff101373fb9a8086ac8902d6fbd82
Christian Valladares <ngdz01@gmail.com>
2015-07-01 15:01:09 -0600

    Inicia
    Login

---
+++ evn/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\christian.valladares\Desktop\holadjango\evn\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
